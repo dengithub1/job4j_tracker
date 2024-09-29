c88a56dcd11f7bd9557d19d9a9592cd5cc0c81c6
admin <desantt8@gmail.com>
2024-09-29 17:27:10 +0300

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,11 @@
+package ru.job4j.pojo;
+
+public class College {
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFio("Smirnov Andrey Ivanovich");
+        student.setGroup("MT-16");
+        student.setDate("24.12.1990");
+    }
+}
