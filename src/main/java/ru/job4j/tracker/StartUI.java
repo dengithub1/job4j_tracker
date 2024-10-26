d87f61d7292a0cf4d2714fc6b3b4bb06dca62c77
admin <desantt8@gmail.com>
2024-10-26 18:18:14 +0300

    3.6. Реализация класса StartUI. Вывод заявок по имени [#500749]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -60,6 +60,19 @@ public class StartUI {
                 } else {
                     System.out.println("Заявка с ввеленым id: " + id + " не найдена.");
                 }
+            }
+            if (select == 5) {
+                System.out.println("=== Вывод заявок по имени ===");
+                System.out.print("Введите имя: ");
+                String name = scanner.nextLine();
+                Item[] items = tracker.findByName(name);
+                if (items.length > 0) {
+                    for (Item item : items) {
+                        System.out.println(item);
+                    }
+                } else {
+                    System.out.println("Заявки с именем: " + name + " не найдены.");
+                }
             } else if (select == 6) {
                 run = false;
             }
