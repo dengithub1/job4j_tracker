a6a9eca8e95ed86236d4cbe8c4b65063bb63724a
admin <desantt8@gmail.com>
2024-11-11 13:36:18 +0300

    0. Map, HashMap [#212652 #521046]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("gena@mail.ru", "Иванов Геннадий Иванович");
+        map.put("kolya@mail.ru", "Котов Николай Иванович");
+        map.put("kolya@mail.ru", "Котов Николай Николаевич");
+        map.put("dima@mail.ru", "Котов Дмитрий Сергеввич");
 
         for (String key : map.keySet()) {
             System.out.println(key + " = " + map.get(key));
